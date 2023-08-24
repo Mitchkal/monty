c22374062dd92c3f33400b36485e3b083fa8cf74
Mitchkal <mitchellkalenda@gmail.com>
2023-08-24 19:43:08 -0400

    fixed mul error

--- calculators.c
+++ calculators.c
@@ -88,7 +88,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		flag = 2;
 	}
 	else
